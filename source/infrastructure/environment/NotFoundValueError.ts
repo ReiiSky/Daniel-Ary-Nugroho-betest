db1ed74ebe7fd79911a95240a8e9258cfaa754a5
ReiiSky <danielarya185@gmail.com>
2024-04-18 00:32:13 +0700

    add: remove unused error class

--- source/infrastructure/environment/NotFoundValueError.ts
+++
@@ -1,7 +0,0 @@
-import {NonRecoverableError} from '../../package/NonRecoverableError';
-
-export class NotFoundValueError extends NonRecoverableError {
-  constructor(key: string) {
-    super(`retrieve '${key}' env value not found error.`);
-  }
-}
